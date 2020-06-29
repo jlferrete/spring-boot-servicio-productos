477215d26eb1191b1a096f845f11dd158494bf7e
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-29 14:27:46 +0200

    Connecting our services as Eureka customers

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,9 @@ public class ProductoController {
 	@Autowired
 	private Environment env;
 	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@Autowired
 	private IProductoService productoService;
 	
@@ -25,7 +29,8 @@ public class ProductoController {
 	public List<Producto> listar() {
 		
 		return productoService.findAll().stream().map(producto -> {
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -33,7 +38,8 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return producto;
 	}
 	
